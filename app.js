910efdb1d9821ede91b8c2449f56b58f6d4f6d80
DINH VU Hien <dinhvuhien@MacBook-Pro-de-DINH.local>
2020-05-22 22:33:06 +0200

    starting files

---
+++ app.js
@@ -0,0 +1,17 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res)=>{
+  res.render("index", );
+});
+
+app.listen(3000, ()=>{
+  console.log("server on 3000 port ...");
+})

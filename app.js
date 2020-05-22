c737c8b1ea0faa5fe35af6900045176772bab1f9
DINH VU Hien <dinhvuhien@MacBook-Pro-de-DINH.local>
2020-05-22 22:53:39 +0200

    add folder public

--- app.js
+++ app.js
@@ -5,11 +5,16 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.set("view engine", "ejs");
 
 app.get("/", (req, res)=>{
-  res.render("index", );
+
+
+
+  res.render("index", {joke1: "hello", joke2: "hello again"});
 });
 
 app.listen(3000, ()=>{

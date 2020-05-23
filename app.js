86804822c450e754c46a9f14bf6e8ca892f8b8b0
DINH VU Hien <dinhvuhien@MacBook-Pro-de-DINH.local>
2020-05-23 12:59:18 +0200

    transfert code in server and GOOD

--- app.js
+++ app.js
@@ -14,37 +14,99 @@ app.get("/", function (req, res) {
 
   https.get(url, function (response) {
 
+    let body = "";
+
     response.on("data", function (data) {
-      try {
-        let d = JSON.parse(data);
-      }
-      catch (e) {
-        console.log(e);
-      }
-      console.log("before: " + data);
-      const dataJson = JSON.parse(data);
+
+      body += data;
+
+
+
+      // let stringE = "";
+      // try {
+      //   let d = JSON.parse(data);
+      // }
+      // catch (e) {
+      //   console.log(e);
+      //   stringE = e;
+      // }
+      // console.log("before: " + data);
+
+
+      // res.render("index", {joke1 : data, joke2: stringE});
+
+
+
+
+
+      // const dataJson = JSON.parse(data);
+
+      // const type = dataJson.type;
+
+      // if (type === "single") {
+      //   res.render("index", {
+      //     joke1: dataJson.joke.replace(/(\n)+/g, "<br><br>"),
+      //     joke2: ""
+      //   });
+      // }
+      // else if (type === "twopart") {
+      //   res.render("index", {
+      //     joke1: dataJson.setup.replace(/(\n)+/g, "<br>"),
+      //     joke2: dataJson.delivery.replace(/(\n)+/g, "<br>")
+      //   });
+      // }
+      // else {
+      //   console.log("Unknow type of joke");
+      // }
+
+    });
+
+
+    response.on("end", function () {
+
+      const dataJson = JSON.parse(body);
 
       const type = dataJson.type;
 
       if (type === "single") {
+        const stringJoke1 = dataJson.joke.replace(/(\n)+/g, '<br><br>');
+
+        const htmlJoke1 = "<p class='command line2'>" +
+          stringJoke1 + "<span class='cursor2fast'>_</span></p>"+
+          "<p class='command line3fast'>><span class='cursor3fast'>_</span></p>";
+
         res.render("index", {
-          joke1: dataJson.joke.replace(/(\n)+/g, "<br><br>"),
+          joke1: htmlJoke1,
           joke2: ""
         });
       }
       else if (type === "twopart") {
+        const stringJoke1 = dataJson.setup.replace(/(\n)+/g, '<br><br>');
+        const stringJoke2 = dataJson.delivery.replace(/(\n)+/g, '<br><br>');
+
+        const htmlJoke1 = "<p class='command line2'>" +
+        stringJoke1 + "<span class='cursor2'>_</span></p>";
+
+        const htmlJoke2 = "<p class='command line3'>" +
+        stringJoke2 + "<span class='cursor3'>_</span></p>"+
+        "<p class='command line4'>><span class='cursor4'>_</span></p>";
+
+
         res.render("index", {
-          joke1: dataJson.setup.replace(/(\n)+/g, "<br>"),
-          joke2: dataJson.delivery.replace(/(\n)+/g, "<br>")
+          joke1: htmlJoke1,
+          joke2: htmlJoke2
         });
       }
       else {
         console.log("Unknow type of joke");
       }
-
     });
 
   });
+
+
+
+
 
 });
 

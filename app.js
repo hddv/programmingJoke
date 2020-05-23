67c9779fa7e8116d3cbc468ff3dd4c8e1b3858b1
DINH VU Hien <dinhvuhien@MacBook-Pro-de-DINH.local>
2020-05-23 02:39:00 +0200

    add to heroku finish css

--- app.js
+++ app.js
@@ -1,22 +1,53 @@
 //jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
+const https = require("https");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-
 app.set("view engine", "ejs");
 
-app.get("/", (req, res)=>{
+app.get("/", function (req, res) {
+  const url = "https://sv443.net/jokeapi/v2/joke/Programming";
 
+  https.get(url, function (response) {
 
+    response.on("data", function (data) {
+      try {
+        let d = JSON.parse(data);
+      }
+      catch (e) {
+        console.log(e);
+      }
+      console.log("before: " + data);
+      const dataJson = JSON.parse(data);
 
-  res.render("index", {joke1: "hello", joke2: "hello again"});
+      const type = dataJson.type;
+
+      if (type === "single") {
+        res.render("index", {
+          joke1: dataJson.joke.replace(/(\n)+/g, "<br>"),
+          joke2: ""
+        });
+      }
+      else if (type === "twopart") {
+        res.render("index", {
+          joke1: dataJson.setup.replace(/(\n)+/g, "<br>"),
+          joke2: dataJson.delivery.replace(/(\n)+/g, "<br>")
+        });
+      }
+      else {
+        console.log("Unknow type of joke");
+      }
+
+    });
+
+  });
+
 });
 
-app.listen(3000, ()=>{
-  console.log("server on 3000 port ...");
-})
+app.listen(process.env.PORT || 3000, () => {
+  console.log("server ...");
+});
